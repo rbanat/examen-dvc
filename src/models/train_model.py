4f008da054e02a8cc85278d757c86d27a90c68d2
Rizlene Banat <grizzly@MacBook-Air-de-Rizlene.local>
2026-03-13 20:20:47 +0100

    train model

---
+++ src/models/train_model.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+
+PROCESSED_DIR = os.path.join("data", "processed_data")
+MODELS_DIR = "models"
+
+# Chargement des jeux de données
+X_train = pd.read_csv(os.path.join(PROCESSED_DIR, "X_train_scaled.csv"))
+y_train = pd.read_csv(os.path.join(PROCESSED_DIR, "y_train.csv")).squeeze()
+
+# Chargement des meilleurs hyperparamètres
+params_path = os.path.join(MODELS_DIR, "best_params.pkl")
+with open(params_path, "rb") as f:
+    best_params = pickle.load(f)
+
+print(f"Paramètres utilisés pour l'entraînement : {best_params}")
+
+# Entrainement
+model = RandomForestRegressor(**best_params, random_state=42)
+model.fit(X_train, y_train)
+
+print("Entraînement terminé.")
+
+# Sauvegarde
+model_path = os.path.join(MODELS_DIR, "model.pkl")
+with open(model_path, "wb") as f:
+    pickle.dump(model, f)
+
+print(f"Modèle sauvegardé dans '{model_path}'")
